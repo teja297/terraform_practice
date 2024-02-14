d53f3307dd0a5561b9d1221fd4e63f35f2be2392
teja297 <143919845+teja297@users.noreply.github.com>
2024-02-14 22:19:08 +0530

    Create 05rt.tf

---
+++ 05rt.tf
@@ -0,0 +1,25 @@
+resource "aws_route_table" "public_route_table_1" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_igw.id
+  }
+
+  tags = {
+    Name = "Public Route Table 1"
+  }
+}
+
+resource "aws_route_table" "public_route_table_2" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_igw.id
+  }
+
+  tags = {
+    Name = "Public Route Table 2"
+  }
+}
