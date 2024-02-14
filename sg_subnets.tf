e36bc789c54a0206e83ab82b86080df54a881686
teja297 <143919845+teja297@users.noreply.github.com>
2024-02-14 22:17:06 +0530

    Create sg_subnets.tf

---
+++ sg_subnets.tf
@@ -0,0 +1,75 @@
+resource "aws_security_group" "public_subnet_sg_1" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Public Subnet Security Group 1"
+  }
+}
+
+resource "aws_security_group" "public_subnet_sg_2" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Public Subnet Security Group 2"
+  }
+}
+
+# Create security group for load balancer
+
+resource "aws_security_group" "elb_sg" {
+  vpc_id      = aws_vpc.example_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    description = "HTTP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
