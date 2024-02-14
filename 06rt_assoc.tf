d4ea765801804dc422e7614eec94f2f74b7aa649
teja297 <143919845+teja297@users.noreply.github.com>
2024-02-14 22:19:44 +0530

    Create 06rt_assoc.tf

---
+++ 06rt_assoc.tf
@@ -0,0 +1,9 @@
+resource "aws_route_table_association" "public_subnet_association_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table_1.id
+}
+
+resource "aws_route_table_association" "public_subnet_association_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table_2.id
+}
